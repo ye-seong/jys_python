da32d5b91cc2e5558aa977ee669f5f9c02cf09cf
ye-seong <dptjd0109@gmail.com>
2025-03-06 17:20:48 +0900

    배아픔;

--- tkinter_coc_helper/Coc_code.py
+++ tkinter_coc_helper/Coc_code.py
@@ -10,25 +10,50 @@ class Coc(tk.Tk):
         self.geometry('400x500')
         self.resizable(False, False)
 
-        self.rowF1 = tk.Frame(self)
-        self.rowF2 = tk.Frame(self)
+        self.barF = tk.Frame(self)
+        self.desc_rowF1 = tk.Frame(self)
+        self.desc_rowF2 = tk.Frame(self)
+        self.bup_rowF1 = tk.Frame(self)
 
-        self.text = Text(self.rowF1, height=10, width=55)
-        self.turnBtn = Button(self.rowF1, text='실행', command=self.Turn)
-        self.turnBtn1 = Button(self.rowF1, text='실행1', command=self.Turn)
-        self.result = Text(self.rowF2, height=10, width=55)
+        self.descBtn = Button(self.barF, text='/desc삽입', command=self.DescMode)
+        self.backupBtn = Button(self.barF, text='백업헬퍼', command=self.BackupMode)
+
+        self.text = Text(self.desc_rowF1, height=10, width=55)
+        self.turnBtn = Button(self.desc_rowF1, text='실행', command=self.Turn)
+        self.turnBtn1 = Button(self.desc_rowF1, text='또먼기능하지', command=self.Turn)
+        self.result = Text(self.desc_rowF2, height=10, width=55)
+
+        self.buptext = Text(self.bup_rowF1, height=10, width=55)
+        self.sizeLabel = Label(self.bup_rowF1, text='박스 사이즈를 px을 포함하여 작성해주세요.')
+        self.sizeEntry = Entry(self.bup_rowF1)
 
     def initWindow(self):
-        self.rowF1.pack()
-        self.rowF2.pack()
+        self.barF.pack()
+        self.desc_rowF1.pack()
+        self.desc_rowF2.pack()
+        self.descBtn.pack(side=LEFT)
+        self.backupBtn.pack(side=LEFT)
         self.text.pack(pady=10)
-        self.turnBtn.pack(side=LEFT, padx=5, pady=10)
-        self.turnBtn1.pack(side=LEFT, padx=5, pady=10)
+        self.buptext.pack()
+        self.sizeLabel.pack()
+        self.sizeEntry.pack()
+        self.turnBtn.pack(side=LEFT, ipadx=30, ipady=1, padx=50, pady=10)
+        self.turnBtn1.pack(side=RIGHT, ipadx=30, ipady=1, padx=50, pady=10)
         self.result.pack()
+
+    def DescMode(self):
+        self.desc_rowF1.pack()
+        self.desc_rowF2.pack()
+        self.bup_rowF1.forget()
+
+    def BackupMode(self):
+        self.desc_rowF1.forget()
+        self.desc_rowF2.forget()
+        self.bup_rowF1.pack()
 
     def Turn(self):
         text = self.text.get('1.0', 'end-1c')
-        arr = re.findall(r'.*?\.+|\S.*?(?=\n|$)', text)
+        arr = re.findall(r'[^.!?\n]+[.!?]+|[^\n]+', text)
         result = ''
         for i in arr:
             if i[0] == ' ':
